a23ded4770f417e299eea4b8550d2896d9dc76d5
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-03-08 21:11:19 +0900

    📝 삼총사(문제) :  3중 for문 사용하기

---
+++ 2023 CodingTest/삼총사.c
@@ -0,0 +1,29 @@
+// 문제 설명
+
+/* 
+    학교 학생 3명의 정수 번호를 더했을 때 0이 되면 3명의 학생은 삼총사입니다.
+    예를 들어, 5명의 학생이 있고, 각각의 정수 번호가 순서대로 -2, 3, 0, 2, -5일 때, 
+    첫 번째, 세 번째, 네 번째 학생의 정수 번호를 더하면 0이므로 세 학생은 삼총사입니다.
+*/
+
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// number_len은 배열 number의 길이입니다.
+int solution(int number[], size_t number_len) {
+    int answer = 0;
+    for (int i = 0; i < number_len - 2; i++) {
+        for (int j = i + 1; j < number_len - 1; j++) {
+            for (int k = j + 1; k < number_len; k++) {
+                if ((number[i] + number[j] + number[k]) == 0) {
+                    answer++;
+                    break;
+                }
+            }
+        }
+    }
+
+    return answer;
+}
