335c2d6179e81ee36705594628855cff7c49861d
SEOYEON BAE <83990943+seoyeon-double-7@users.noreply.github.com>
2023-03-08 21:24:06 +0900

    ✅ 삼총사(문제) : for문 조건 number_len까지로 수정

--- 2023 CodingTest/Lv1/삼총사.c
+++ 2023 CodingTest/Lv1/삼총사.c
@@ -6,7 +6,6 @@
     첫 번째, 세 번째, 네 번째 학생의 정수 번호를 더하면 0이므로 세 학생은 삼총사입니다.
 */
 
-
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -14,17 +13,18 @@
 // number_len은 배열 number의 길이입니다.
 int solution(int number[], size_t number_len) {
     int answer = 0;
-    for (int i = 0; i < number_len; i++) {
-        for (int j = i + 1; j < number_len; j++) {
-            for (int k = j + 1; k < number_len; k++) {
-                if ((number[i] + number[j] + number[k]) == 0) {
+    int i, j, k;
+    int sum = 0;
+    for (i = 0; i < number_len; i++) {
+        for (j = i + 1; j < number_len; j++) {
+            for (k = j + 1; k < number_len; k++) {
+                sum = number[i] + number[j] + number[k];
+                if (sum == 0) {
                     answer++;
-                    break;
                 }
             }
         }
     }
 
     return answer;
-}
 }
